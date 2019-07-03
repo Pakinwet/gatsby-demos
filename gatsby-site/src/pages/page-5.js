f9fbf90672b14adb46091bbbbd6ad61d8a0459a5
Ward Peeters <ward@coding-tech.com>
2019-07-03 16:21:55 +0200

    add image to page-5

--- gatsby-site/src/pages/page-5.js
+++ gatsby-site/src/pages/page-5.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page five" />
     <h1>Hi from the fifth page</h1>
     <p>Welcome to page 5</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
     <ul>
       <li><Link to="/">Go to homepage</Link></li>
       <li><Link to="/page-2/">Go to page 2</Link></li>
