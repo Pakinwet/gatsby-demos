a8c66746c79ab9a3f6f53ad198a372543efd451b
Ward Peeters <ward@coding-tech.com>
2019-07-15 10:26:12 +0200

    disable hovering

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -6,3 +6,13 @@
 
 // You can delete this file if you're not using it
 exports.disableCorePrefetching = () => true
+
+
+// We override loadPage & loadPagesync to fix canonical redirects
+// we also override hovering to disable hover prefetch
+exports.onClientEntry = () => {
+  const loader = window.___loader;
+
+  // disable hovering prefetching as we don't know if we can.
+  loader.hovering = () => { };
+}
